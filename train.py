e6c25997bfcd13d1964f7f862575585ff004dd5f
yoavharlap <yoavharlap1@gmail.com>
2023-11-08 18:52:54 +0200

    111212121212

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ else:
 # learning_rates = [1e-5, 1e-6, 1e-7]
 # num_epochs_list = [40, 60, 80]
 learning_rates = [1e-4,1e-5,1e-6]
-learning_rates = [1e-6]
+#learning_rates = [1e-6]
 num_epochs_list = [300]
 
 # Initialize an index counter
